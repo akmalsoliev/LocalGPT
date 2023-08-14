96e83536fd1db7562b10c31240df427bef71bba9
Akmal Soliev <soliev@gmail.com>
2023-08-15 00:53:10 +0500

    [FEAT] Sidebar with historical chat

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -2,3 +2,4 @@ from .streamlit_init import streamlit_start
 from .check import check_files
 from .greetings_generator import greetings
 from .display_message import display_messages
+from .save import save_messages
--- src/util/streamlit_init.py
+++ src/util/streamlit_init.py
@@ -1,8 +1,52 @@
 import streamlit as st
+import os
 
-def streamlit_start():
+
+def streamlit_start(greetings_msg):
     st.set_page_config(
         page_title="Friendly Chatbot",
         page_icon="🤖"
     )
     st.header("Your own ChatGPT 🤖")
+
+    MODELS = ["gpt-3.5-turbo", "gpt-4"]
+    CHAT_PATH = "io/chat"
+
+    with st.sidebar:
+
+        # New Chat Button
+        if st.button("New Chat"):
+            st.session_state.messages = [greetings_msg]
+            if "chat_selection" in st.session_state:
+                del st.session_state["chat_selection"]
+        st.divider()
+
+        # Model selection 
+        st.session_state.model = st.selectbox(
+            "Select the GPT model",
+            MODELS
+        )
+        st.write("Selected model: ", st.session_state.model)
+        
+        # Historical Interactions
+        if os.path.exists(CHAT_PATH):
+            files = []
+            for _, _, f in os.walk(CHAT_PATH):
+                if f:
+                    files = f
+                    files.reverse()
+
+            if len(files) > 0:
+                st.divider()
+                st.write("Historical Chat:")
+
+            if len(files) > 20:
+                delete_file = files[-1]
+                path_delete_file = os.path.join(CHAT_PATH, delete_file)
+                os.remove(path_delete_file)
+
+            for file in files:
+                clean_name = file.replace(".json", "")
+                if st.button(clean_name):
+                    file_path = os.path.join(CHAT_PATH, file)
+                    st.session_state["chat_selection"] = file_path

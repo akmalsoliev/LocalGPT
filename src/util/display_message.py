655a7282e1a2d3f76a40a63944f48a3df6c6a0d1
Akmal Soliev <soliev@gmail.com>
2023-09-04 15:26:27 +0200

    [FEAT] Added ability to export to Markdown

--- src/util/display_message.py
+++ src/util/display_message.py
@@ -1,3 +1,4 @@
+import os 
 import streamlit as st 
 from langchain.schema import (
     AIMessage,
@@ -5,10 +6,17 @@ from langchain.schema import (
 )
 
 def display_messages():
-    for message in st.session_state.messages:
+    for index, message in enumerate(st.session_state.messages):
         if type(message) in [AIMessage]:
             with st.chat_message("assistant"):
                 st.write(message.content)
+            if index > 2:
+                if st.button("Export Markdown"):
+                    name = message.content[:30]
+                    file_name = os.path.join("io", "markdown", f"{name}.json")
+                    with open(file_name, "w") as f:
+                        f.write(message.content)
+
         elif type(message) == HumanMessage:
             with st.chat_message("user"):
                 st.write(message.content)
